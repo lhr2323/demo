7110a11bb3b6e2db6274de6339edd7f455193d53
lianghairui <2323155449@qq.com>
2020-10-12 14:32:15 +0800

    更改

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,6 +15,6 @@ public class App
         System.out.println("创建分支");
         System.out.println("分支添加");
         System.out.println("主干添加");
-        System.out.ptintln("在线编辑");
+        System.out.println("在线编辑");
     }
 }
